2a6f582cd64ce6fb9fa7f33a81a0edd2b95f0dca
Md Sahil <287mdsahil@gmail.com>
2020-04-09 12:08:55 +0530

    Handled final states

--- src/algorithms/SubsetConstruction.java
+++ src/algorithms/SubsetConstruction.java
@@ -19,7 +19,6 @@ public class SubsetConstruction {
 
     int start = nfa.getStartState();
     Set<Character> alphabets = nfa.getAlphabets();
-    System.out.println(alphabets);
 
     // Initialization
     Map<Integer, Set<Integer>> subsets = new TreeMap<>();
@@ -35,62 +34,71 @@ public class SubsetConstruction {
 
       for (Character a : alphabets) {
         Set<Integer> u = nfa.epsilonClosure(nfa.move(cur_nfa_states, a));
-        System.out.println("Ec:" + u + " of:" + cur_nfa_states + " on:" + a);
         if (!u.isEmpty()) {
           Integer new_dfa_state = null;
           if (!subsets.containsValue(u)) {
             new_dfa_state = subsets.size();
             subsets.put(new_dfa_state, u);
             unmarked_dfa_states.add(new_dfa_state);
-            System.out.print("New Subset:");
-            System.out.println(u);
 
             dfa.increaseNumberOfStates(1);
+
+            //check if new state is a final state
+            boolean is_final = false;
+            for(int nfa_state : u) {
+              if(nfa.getFinalStates().contains(nfa_state)) {
+                is_final = true;
+                break;
+              }
+            }
+            if(is_final) {
+              Set<Integer> dfa_final_states = dfa.getFinalStates();
+              dfa_final_states.add(new_dfa_state);
+              dfa.setFinalStates(dfa_final_states);
+            }
           } else {
-	    for(Integer k : subsets.keySet()) {
-              if(subsets.get(k).equals(u)) {
+            for (Integer k : subsets.keySet()) {
+              if (subsets.get(k).equals(u)) {
                 new_dfa_state = k;
                 break;
-              }              
-	    }
+              }
+            }
           }
 
           dfa.addTransition(cur_dfa_state, a, new_dfa_state);
-          System.out.print("Transition added:");
-          System.out.println("From:" + cur_dfa_state + " to:" + new_dfa_state + " on:" + a);
         }
       }
-
-      System.out.println("Unmarked:" + unmarked_dfa_states);
     }
-
-    System.out.println(subsets);
-
     return dfa;
   }
 
   public static void main(String[] args) {
-        NFA nfa = new NFA(5, 0, Arrays.asList(2, 4));
+    NFA nfa = new NFA(5, 0, Arrays.asList(2, 4));
 
-        nfa.addEpsilonTransition(0, 1);
-        nfa.addEpsilonTransition(0, 3);
-        nfa.addNormalTransition(1, 'a', 2);
-        nfa.addNormalTransition(2, 'a', 2);
-        nfa.addNormalTransition(3, 'b', 4);
-        nfa.addNormalTransition(4, 'b', 4);
-        nfa.reset();
+    nfa.addEpsilonTransition(0, 1);
+    nfa.addEpsilonTransition(0, 3);
+    nfa.addNormalTransition(1, 'a', 2);
+    nfa.addNormalTransition(2, 'a', 2);
+    nfa.addNormalTransition(3, 'b', 4);
+    nfa.addNormalTransition(4, 'b', 4);
+    nfa.reset();
 
     DFA dfa = convert(nfa);
 
-    System.out.println(dfa.getNumberOfStates());
-    System.out.println(dfa.transitions);
-
+    System.out.println("Dfa no of states:" + dfa.getNumberOfStates());
+    System.out.println("Dfa transitions:" + dfa.transitions);
+    System.out.println("Dfa final states:" + dfa.getFinalStates());
 
     for (int i = 0; i < args[0].length(); i++) {
-      dfa.advance(args[0].charAt(i));
 
       System.out.println(dfa.currentState);
-      // System.out.println();
+      dfa.advance(args[0].charAt(i));
+      System.out.println(args[0].charAt(i));
+      System.out.println(dfa.currentState);
+      System.out.println();
     }
+    if(dfa.isInFinalState()) 
+      System.out.println("String accepted");
+    else System.out.println("String rejected");
   }
 }
