04868d1693db78f764a91a93683fa949770db091
pmcarpan <32287473+pmcarpan@users.noreply.github.com>
2020-04-09 15:46:46 +0530

    remove unused packages from Thompson.java

--- src/algorithms/Thompson.java
+++ src/algorithms/Thompson.java
@@ -2,10 +2,7 @@ package algorithms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
