a2ff7c95b1371c8b6a38061e2f39b4547e25bc55
pmcarpan <32287473+pmcarpan@users.noreply.github.com>
2020-04-29 20:58:52 +0530

    added row,col in lexer and tokens

--- src/lexer/LexToken.java
+++ src/lexer/LexToken.java
@@ -1,12 +1,22 @@
 package lexer;
 
 public class LexToken {
-    public final int id;
+    public final int id, row, col;
     public final String tokenType, lexeme;
 
-    public LexToken(int id, String tokenType, String lexeme) {
+    public LexToken(int id, int row, int col, String tokenType, String lexeme) {
         this.id = id;
+        this.row = row;
+        this.col = col;
         this.tokenType = tokenType;
         this.lexeme = lexeme;
+    }
+
+    public static String getFormattedHeading() {
+        return String.format("%5s %5s %5s %-15s%s", "ID", "ROW", "COL", "TYPE", "LEXEME");
+    }
+
+    public String getFormattedString() {
+        return String.format("%5d %5d %5d %-15s%s", id, row, col, tokenType, lexeme);
     }
 }
